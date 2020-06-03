557c5519b815e266a7ba1ba4a6906f3ab6d77fa8
viviantom0506 <icemarina123@gmail.com>
2020-06-02 20:50:34 -0400

    Added proptypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ContactCard from "./ContactCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'; 
+
+class App extends Component {
+  render() {
+    return (
+      <> 
+      <ContactCard name = "Baekhyun"
+      mobile = "3471231235"
+      work = "9179007653"
+      email = "byunbaekhyun@gmail.com"
+      />
+      <ContactCard name = "Kai"
+      mobile = "3475677788"
+      work = "7187651267"
+      email = "kimkai@gmail.com"
+      />
+      <ContactCard name = "Sehun"
+      mobile = "9176325632"
+      work = ""
+      email = "sehunnieOh@gmail.com" 
+      />
+      </>
+    )
+  }
 }
 
 export default App;
